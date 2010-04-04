8a261c04328c9bfa87d125d4c56c0928ecc1b8ea
Seivan Heidari <seivan@kth.se>
2010-04-04 14:32:52 +0200

    Added formtastic initializer, but removed formtastic css

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,17 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-
+  config.gem "searchlogic", :source => 'http://gemcutter.org'
+  config.gem "cancan", :source => 'http://gemcutter.org'
+  config.gem "RedCloth", :source => 'http://gemcutter.org'
+  config.gem "annotate", :source => 'http://gemcutter.org'
+  config.gem "nifty-generators", :lib => false, :source => 'http://gemcutter.org'
+  config.gem "authlogic", :source => 'http://gemcutter.org'
+  config.gem "acts-as-taggable-on", :source => 'http://gemcutter.org'
+  config.gem "paperclip", :source => 'http://gemcutter.org'
+  config.gem "haml", :lib => false, :source => 'http://gemcutter.org'
+  config.gem "compass", :lib => false, :source => 'http://gemcutter.org'
+  config.gem "fancy-buttons", :lib => false, :source => 'http://gemcutter.org'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
