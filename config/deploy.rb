af3fc3542cba768997977a54c3c85cde926e5aae
Seivan Heidari <seivan@kth.se>
2010-04-05 19:38:39 +0200

    modified deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,31 +1,41 @@
 set :application, "blogstain"
 set :user, "seivan"
-default_run_options[:pty] = true
-
-set :repository,  "git@github.com:seivan/blogstain.git"
-ssh_options[:forward_agent] = true
-set :branch, "master"
-set :deploy_to, "/var/www/apps/#{application}"
-
-set :scm, :git 
-#:scm, :subversion # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :domain, "debby.zapto.org"
-role :web, domain                          # Your HTTP server, Apache/etc
-role :app, domain  #"your app-server here"                          # This may be the same as your `Web` server
-#role :db,  domain  #"your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,   domain, :primary => true  #"your slave db-server here"
+role :web, domain      
+role :app, domain  
+role :db,   domain, :primary => true
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :scm, "git"
 set :scm_username, user
 set :runner, user
-
 set :rails_env, "production"
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :repository,  "git@github.com:seivan/blogstain.git"
+set :deploy_to, "~/#{application}"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
+set :git_enable_submodules, 1
 
-# namespace :deploy do
-#   task :start do ; end
+namespace :deploy do
+  desc "Restart the server (Passenger) - need to change to unicorn+nginx"
+  task :start do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  desc "Symlink shared configs and folders on each release"
+  task :symlink_shared do 
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config.database.yml"
+  end
+  desc "Sync the public/assets directory"
+  # task :pictures do 
+  #   system "scp -r pictures #{user}@#{domain}:#{release_path}/public/images"
+  # end
+end
 #   task :stop do ; end
 #   task :restart, :roles => :app, :except => { :no_release => true } do
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+#After deploy, run symlink_shared
+after 'deploy:updated_code', 'deploy:symlink_shared'
