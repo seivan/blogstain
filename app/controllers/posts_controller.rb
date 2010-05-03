3864502d90d7d2b86b2d8dd835b23b8390eb8e6a
Seivan Heidari <seivan@kth.se>
2010-05-03 06:41:10 +0200

    added created_at_desc for the @archives removed the content folder from the views, moved some stuff to admin/shared and shared/

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
  # authorize_resource
   respond_to :html, :json, :atom, :js
   before_filter do
-      @archives ||= Post.published.archives
+      @archives ||= Post.published.archives.created_at_desc
   end
   def index
     @posts = Post.get_posts_or_archive(params)
