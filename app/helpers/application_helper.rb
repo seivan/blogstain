8b4a89379c4298597c68a90e8a9d7b76118e69b1
Seivan Heidari <seivan@kth.se>
2010-05-03 08:20:23 +0200

    Decided to show updated_at when something has been updated combined with created_at (published)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
       image_tag('http://www.gravatar.com/avatar/' + Digest::MD5.hexdigest(email), :class => "gravatar")
     end
     
-    def published_at(current_time_shown) 
+    def readable_time(current_time_shown) 
       if current_time_shown >= (Time.zone.now + 7.days)
         current_time_shown.strftime("%Y-%m-%d %H:%M")
       else
