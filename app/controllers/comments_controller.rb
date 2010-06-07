1cdccbe68a5417c95daa753596f75ce44dd86058
Seivan Heidari <seivan@kth.se>
2010-06-08 01:34:56 +0200

    Form added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -41,12 +41,13 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.xml
   def create
-    @comment = Comment.new(params[:comment])
+    @commentable = find_commentable
+    @comment = @commentable.comments.build(params[:comment])
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to(@comment, :notice => 'Comment was successfully created.') }
-        format.xml  { render :xml => @comment, :status => :created, :location => @comment }
+        format.html { redirect_to :id => nil }
+        #format.xml  { render :xml => @comment, :status => :created, :location => @comment }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
